24736dbdbe0940e275fcdfff87afc87ac5da0466
Michal Rozycki <michalrozyck@gmail.com>
2016-05-20 11:35:30 +0200

    Utilities for DB testing

---
+++ src/main/java/com/epam/DAO/DBmanagment/DBUtilities.java
@@ -0,0 +1,7 @@
+package com.epam.DAO.DBmanagment;
+
+/**
+ * Created by michalr on 19.05.16.
+ */
+public interface DBUtilities {
+}
---
+++ src/test/java/com/epam/DB/DBTestsUtil.java
@@ -0,0 +1,7 @@
+package com.epam.DB;
+
+/**
+ * Created by michalr on 19.05.16.
+ */
+public class DBTestsUtil {
+}
